54f0f59e19c3c3a339af9104bb0ca0fa21d3eaa9
nuelcyoung <33932707+nuelcyoung@users.noreply.github.com>
2022-08-24 11:26:42 +0100

    main change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:training/routes/router.dart';
 import 'package:training/utils/textscale.dart';
 
@@ -26,38 +25,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  State<RandomWords> createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final _suggestions = <WordPair>[];
-    const _biggerFont = TextStyle(fontSize: 18.0);
-    return Scaffold(
-      body: SafeArea(child: 
-      ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemBuilder: /*1*/ (context, i) {
-            if (i.isOdd) return const Divider(); /*2*/
-
-            final index = i ~/ 2; /*3*/
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-            }
-            return ListTile(
-              title: Text(
-                _suggestions[index].asPascalCase,
-                style: _biggerFont,
-              ),
-            );
-          },
-        ),
-      )
-    );
-  }
-}
